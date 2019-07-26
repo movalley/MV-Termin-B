c9757f29011f4d1a539f801bf04f6c83dbdb451a
Mario <rozic.mario@hotmail.com>
2019-07-26 16:02:37 +0200

    :sparkles: Auth middleware

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var profileRoute = require('./routes/profile');
 
 let mongoPass = process.env.MongoPass
 
@@ -28,6 +30,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', usersRouter);
+
+// auth middleware
+app.use((req,res, next) => {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if(token) {
+      jwt.verify(token, process.env.SuperSecret, (err, decoded) => {
+          if(err) {
+              return res.status(401).json({ message: 'Fail to authenticate token. You shall not pass!'});
+          } else {
+              req.decoded = decoded;
+              next();
+          }
+      });
+  } else {
+      return res.status(401).send({
+          message: 'No token provided. You shall not pass!'
+      });
+  }
+})
+
+app.use('/profile', profileRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

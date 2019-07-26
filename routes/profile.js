52c24646fc5639e7dac007b2aae28e272f69500b
Mario <rozic.mario@hotmail.com>
2019-07-26 17:21:51 +0200

    :sparkles: Database check

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Profile'})
+  res.status(200).json({ msg: 'Profile page'})
 });
 
 module.exports = router;

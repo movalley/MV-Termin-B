7c9011bf556fe5cc99ded929fe8bfe1ed2a96222
Mario <rozic.mario@hotmail.com>
2019-08-01 14:45:27 +0200

    :sparkles: Add user obj in profile response

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.status(200).json({ msg: "Loged in user" });
+  res.status(200).json({ msg: "Loged in user", profile: req.decoded });
 });
 
 module.exports = router;
